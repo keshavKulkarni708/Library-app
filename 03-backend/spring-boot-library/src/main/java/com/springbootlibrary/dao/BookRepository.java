9b2cd6a8e27df229799cf91323e8fb3ab1e5c30d
Keshav <keshav.kulkarni708>
2023-12-03 15:34:09 +0530

    Created a new API Entity

---
+++ 03-backend/spring-boot-library/src/main/java/com/springbootlibrary/dao/BookRepository.java
@@ -0,0 +1,7 @@
+package com.springbootlibrary.dao;
+
+import com.springbootlibrary.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository  extends JpaRepository<Book,Long> {
+}
